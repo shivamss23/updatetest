feb31b804861f74ed2dfb601ebbaef2055dd48ba
shivamss23 <117813077+shivamss23@users.noreply.github.com>
2022-11-09 15:36:31 +0530

    Add files via upload

---
+++ xdlab/settings.js
@@ -0,0 +1,23 @@
+// URL
+exports.url = 'http://192.168.1.20:85';
+
+// Dev Tools
+exports.devTools = 0;
+
+//window width
+exports.w = 1920;
+
+//window height
+exports.h = 1080;
+
+//position x
+exports.Winx = 0;
+
+//position y
+exports.Winy = 0;
+
+//fullscreen  use true or false
+exports.fullscreen = false;
+
+//cache setting. Enter 1 to delete cache
+exports.delCache = 1;
